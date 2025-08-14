0b043f9e3d9ca18c55d42d755639fd6e28ef9b03
rafaelf-elias <rafaelfrreiraelias@gmail.com>
2025-08-14 18:47:52 -0300

    Grafico semanal e comportamento de elementos.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
 console.log("script loaded");
 
-const ctx = document.getElementById('usageChartCanvas');
+const ctx = document.getElementById('dailyUsageChartCanvas');
 
 new Chart(ctx, {
     type: 'doughnut',
     options: {
         cutout: '75%',
-        responsive: false,
-        plugins: {
-            legend: {
-                display: false,
-            },
-        },
-        // ! maintainAspectRatio: true,
+        plugins: { legend: { display: false } },
     },
     data: {
         labels: ['Tiktok', 'Instagram', 'X', 'Reddit', 'Clash Royale'],
@@ -25,4 +19,23 @@ new Chart(ctx, {
         borderWidth: 0
         }],
     },
+});
+
+const ctx2 = document.getElementById('weeklyUsageChartCanvas');
+
+new Chart(ctx2, {
+    type: 'bar',
+    options: {
+        plugins: { legend: { display: false } },
+        scales: { y: { display: false } },
+    },
+    data: {
+        labels: ['Dom.', 'Seg.', 'Ter', 'Qua.', 'Qui.', 'Sex.', 'Sab.'],
+        datasets: [{
+            label: 'Minutos de uso',
+            data: [65, 63, 47, 44, 42, 10, 0],
+            backgroundColor: '#7322DA',
+            borderRadius: 8 
+        }]
+    }
 });
